d8c6b172b9fadc4492a43e68f68e8a1de7faff2c
Lionnel7 <pierrerichan@gmail.com>
2024-11-01 17:23:02 +0100

    numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *print_numbers - function that print all numbers from
 *0 to 9
@@ -10,7 +11,7 @@ void print_numbers(void)
 int number = 48;
 for (number = 48; number < 58; number++)
 {
-_putchar(number);
+putchar(number);
 }
-_putchar(10);
+putchar(10);
 }
