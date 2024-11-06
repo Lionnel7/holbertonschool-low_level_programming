1e52b543a53c1df4091b2d65fb5c1860ca509b02
Lionnel7 <pierrerichan@gmail.com>
2024-11-06 14:46:55 +0100

    strs

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,3 +18,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
