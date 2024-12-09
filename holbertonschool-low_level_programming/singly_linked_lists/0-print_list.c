b26c4383cf9e89c7e756941fb2eeb149dc97a023
Lionnel7 <pierrerichan@gmail.com>
2024-12-09 14:51:39 +0100

    ddd

---
+++ holbertonschool-low_level_programming/singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+* print_list - prints all the elements of a list
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
+*/
+
+size_t print_list(const printf *h)
+{
+const printf *cursor = h;
+size_t count = 0;
+
+while (cursor != NULL)
+{
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next;
+}
+
+return (count);
+}
