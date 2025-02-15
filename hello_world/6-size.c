44183b2a6a5c0533f4b778ac4b481d06006327a1
Lionnel7 <pierrerichan@gmail.com>
2025-02-15 22:37:48 +0000

    list types sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
   * main - Prints the sizeof many var types
   *
@@ -18,4 +19,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
-}	
+}
