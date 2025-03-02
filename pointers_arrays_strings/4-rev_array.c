7b1b3adfb36b2209f4f13b89099dd0c717e0024b
Lionnel7 <pierrerichan@gmail.com>
2025-03-02 22:57:16 +0100

    write

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - inverse le contenu d'un tableau d'entiers.
+ * @a: un tableau d'entiers
+ * @n: le nombre d'éléments du tableau
+ *
+ * Return: rien.
+ */
+void reverse_array(int *a, int n)
+{
+    int i, temp;
+
+    for (i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
+    }
+}
