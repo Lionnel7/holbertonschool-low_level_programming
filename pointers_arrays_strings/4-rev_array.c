dce479902824c0ee193ed339f0b7a64440350a48
Lionnel7 <pierrerichan@gmail.com>
2025-03-02 23:05:22 +0100

    write

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reverse_array - inverse le contenu d'un tableau d'entiers.
- * @a: un tableau d'entiers
- * @n: le nombre d'éléments du tableau
- *
- * Return: rien.
- */
+  * reverse_array - Reverses the content of an array of integers
+  * @a: An array of integers
+  * @n: Number of elements to swap
+  *
+  * Return: empty
+  */
 void reverse_array(int *a, int n)
 {
-        int i, temp;
+	int *p, i, aux, k;
 
-        for (i = 0; i < n / 2; i++)
-        {
-                temp = a[i];
-                a[i] = a[n - 1 - i];
-                a[n - 1 - i] = temp;
-        }
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
 }
