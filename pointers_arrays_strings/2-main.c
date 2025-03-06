8f9fa77cddd0d3abf26749980d95a27d38157ec5
Lionnel7 <pierrerichan@gmail.com>
2025-03-06 02:27:03 +0100

    copies memries

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * main - check the code
@@ -9,39 +8,14 @@
  */
 int main(void)
 {
-    char s1[98];
-    char *ptr;
-    int i;
+    char *s = "hello";
+    char *f;
 
-    for (i = 0; i < 98 - 1; i++)
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
     {
-        s1[i] = '*';
+        printf("%s\n", f);
     }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-
-    ptr = strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    s1[5] = '\0';
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-
-    ptr = strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    s1[90] = '\0'; 
-    printf("%s", s1);
-    printf("%s", ptr);
-
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
     return (0);
 }
